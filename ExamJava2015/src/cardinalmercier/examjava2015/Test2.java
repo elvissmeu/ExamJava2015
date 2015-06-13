ab4e009cbb35476dbf94a99cf820ac7090ac18c7
elvissmeu <elvissmeu@gmail.com>
2015-06-13 07:03:08 +0200

    test 2

---
+++ ExamJava2015/src/cardinalmercier/examjava2015/Test2.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package cardinalmercier.examjava2015;
+
+/**
+ * @author elvissmeu
+ *
+ */
+public class Test2 {
+
+	/**
+	 * 
+	 */
+	public Test2() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
